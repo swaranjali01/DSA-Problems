0c695294d125d5d560bb8a5ae8c208f1fee8cfae
swaranjali01 <swaragawande26@gmail.com>
2024-11-09 13:36:30 +0530

    5TH  problem of array D

--- Array/RotateArray.java
+++ Array/RotateArray.java
@@ -24,5 +24,15 @@ public class RotateArray {
             end--;
         }
     }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};  
+        int k = 3;  
+        rotate(nums, k);
+
+       
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
 }
- 
+  
