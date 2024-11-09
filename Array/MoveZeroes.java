9799be35dd1f5b8a90615d934039f5e928b0dcc5
swaranjali01 <swaragawande26@gmail.com>
2024-11-09 13:44:24 +0530

    6TH  problem  of array D

--- Array/MoveZeroes.java
+++ Array/MoveZeroes.java
@@ -18,4 +18,13 @@ public class MoveZeroes {
         }
     }
 
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12}; 
+        moveZeroes(nums);
+
+        
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
 }
