ae7c412f50934d673cc2ee2301a697185d730a86
swaranjali01 <swaragawande26@gmail.com>
2024-11-10 20:22:27 +0530

    8TH  problem  of array in progress

---
+++ Array/UnionOfSortedArrays.java
@@ -0,0 +1,40 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnionOfSortedArrays {
+
+    public static List<Integer> findUnion(int[] a, int[] b) {
+        List<Integer> result = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < a.length && j < b.length) {
+           
+            if (!result.isEmpty() && result.get(result.size() - 1) == a[i]) {
+                i++;
+                continue;
+            }
+            if (!result.isEmpty() && result.get(result.size() - 1) == b[j]) {
+                j++;
+                continue;
+            }
+
+            if (a[i] < b[j]) {
+                result.add(a[i++]);
+            } else if (a[i] > b[j]) {
+                result.add(b[j++]);
+            } else {
+                result.add(a[i]);
+                i++;
+                j++;
+            }
+        }
+
+       
+       
+        
+    
+
+}
+
