c6273920b7cf67f91a95772cdc77b5f431b42430
swaranjali01 <swaragawande26@gmail.com>
2024-11-11 14:24:55 +0530

    11TH  problem  of array D

--- Array/SingleNumber.java
+++ Array/SingleNumber.java
@@ -12,5 +12,9 @@ public class SingleNumber {
         return single;
     }
 
+    public static void main(String[] args) {
+        int[] nums = {4, 1, 2, 1, 2};  
+        System.out.println("Single number: " + findSingleNumber(nums));
+    }
 }
 
