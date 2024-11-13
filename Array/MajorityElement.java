3cfc0cc0aafa773232a523575b317665f0f103da
swaranjali01 <swaragawande26@gmail.com>
2024-11-13 12:12:20 +0530

    15TH  problem  of array in progress

--- Array/MajorityElement.java
+++ Array/MajorityElement.java
@@ -2,6 +2,20 @@ package Array;
 
 public class MajorityElement {
 
+    public static int findMajorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+
     
 }
 
