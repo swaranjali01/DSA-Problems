5fcc5ce1f61d4c7649766c34eb21f21775eebd71
swaranjali01 <swaragawande26@gmail.com>
2024-11-13 11:59:03 +0530

    14TH  problem  of array in progress

---
+++ Array/SortColors.java
@@ -0,0 +1,14 @@
+package Array; 
+
+public class SortColors {
+
+    public static void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+       
+    }
+
+    
+}
