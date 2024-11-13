6459c4e51552aca74ab89eddca5f44259b8aa3e9
swaranjali01 <swaragawande26@gmail.com>
2024-11-13 12:01:23 +0530

    14TH  problem  of array in progress

--- Array/SortColors.java
+++ Array/SortColors.java
@@ -7,7 +7,25 @@ public class SortColors {
         int mid = 0;
         int high = nums.length - 1;
 
-       
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+              
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+          
+                mid++;
+            } else {
+              
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
     }
 
     
