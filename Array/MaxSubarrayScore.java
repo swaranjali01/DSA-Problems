29a5ee4745dd4751d1be6b00c635a38f67984255
swaranjali01 <swaragawande26@gmail.com>
2024-11-15 15:40:57 +0530

    17TH  problem  of array

--- Array/MaxSubarrayScore.java
+++ Array/MaxSubarrayScore.java
@@ -21,7 +21,9 @@ public class MaxSubarrayScore {
                     }
                 }
 
-        
+                 int score = smallest + secondSmallest;
+
+                 maxScore = Math.max(maxScore, score);
             }
         }
 
