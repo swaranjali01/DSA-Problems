cc2a2730701bb58c3a67e83174cc081468fb5ad9
swaranjali01 <swaragawande26@gmail.com>
2024-11-15 15:25:06 +0530

    17TH  problem  of array

---
+++ Array/MaxSubarrayScore.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class MaxSubarrayScore {
+
+    public static int maxScore(int[] arr) {
+        int n = arr.length;
+        int maxScore = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+               
+
+            }
+        }
+
+        return maxScore;
+    }
+
+}
+
