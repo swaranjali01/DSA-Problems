7cc45230df05fffcbc90c1a9a458dc83304c21dc
swaranjali01 <swaragawande26@gmail.com>
2024-11-16 17:35:54 +0530

    18TH  problem  of array

--- Array/BestTimeToBuyAndSellStock.java
+++ Array/BestTimeToBuyAndSellStock.java
@@ -2,11 +2,20 @@ package Array;
 
 public class BestTimeToBuyAndSellStock {
 
-    public static int maxProfit(int[] ) {
+    public static int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE; 
         int maxProfit = 0;  
         
+        for (int price : prices) {
+
+            minPrice = Math.min(minPrice, price);
+            
+            int profit = price - minPrice;
        
+            maxProfit = Math.max(maxProfit, profit);
+        }
+        
+        return maxProfit;
     }
 
    
