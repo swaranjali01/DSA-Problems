4257daeaee3e32ad1541a22902650d0369bcb875
swaranjali01 <swaragawande26@gmail.com>
2024-11-17 17:06:18 +0530

    19TH  problem  of array

---
+++ Array/RearrangeArray.java
@@ -0,0 +1,21 @@
+package Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RearrangeArray {
+
+    public static int[] rearrangeArray(int[] nums) {
+        List<Integer> positive = new ArrayList<>();
+        List<Integer> negative = new ArrayList<>();
+
+     
+
+
+      
+
+        return result;
+    }
+
+ 
+}
+
