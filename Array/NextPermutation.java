7f1192193453628425b68c79e46aadb80e4c5025
swaranjali01 <swaragawande26@gmail.com>
2024-11-18 18:55:04 +0530

    20TH  problem  of array

---
+++ Array/NextPermutation.java
@@ -0,0 +1,23 @@
+package Array;
+
+import java.util.Arrays;
+
+public class NextPermutation {
+
+    public static void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        
+    }
+
+   
+
+  
+
+  
+}
