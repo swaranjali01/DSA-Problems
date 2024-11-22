592f2093837021a9f709ad76f99400732d277970
swaranjali01 <swaragawande26@gmail.com>
2024-11-22 18:33:55 +0530

    22nd  problem  of array

--- Array/SetMatrixZeroes.java
+++ Array/SetMatrixZeroes.java
@@ -24,7 +24,14 @@ public class SetMatrixZeroes {
             }
         }
 
-       
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0; 
+                    matrix[0][j] = 0; 
+                }
+            }
+        }
 
       
 
