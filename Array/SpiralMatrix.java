5c2c6135ea76a27f5d0fae977d31f4171244d468
swaranjali01 <swaragawande26@gmail.com>
2024-11-25 19:07:51 +0530

    24th  problem  of array

---
+++ Array/SpiralMatrix.java
@@ -0,0 +1,31 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) {
+            return result;
+        }
+
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+        
+           
+
+         
+
+        
+        }
+
+        return result;
+    }
+
+}
+
