441821cd5dd9e018bf61ac3ff072e1a65c9ff215
swaranjali01 <swaragawande26@gmail.com>
2024-11-25 19:24:27 +0530

    24th  problem  of array D

--- Array/SpiralMatrix.java
+++ Array/SpiralMatrix.java
@@ -45,5 +45,14 @@ public class SpiralMatrix {
         return result;
     }
 
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Spiral Order: " + spiralOrder(matrix));
+    }
 }
 
